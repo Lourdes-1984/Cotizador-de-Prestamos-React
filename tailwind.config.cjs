7a063420b44c0dd5a91054c3b4dc3d7dc6b05102
lourdes sosa <lourdes1984.sosa@gmail.com>
2023-03-22 12:08:50 -0300

    instalacion de Tailwind para CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",
+            "./src/**/*.{js,ts,jsk,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
